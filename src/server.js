23ebb4ffcfd79cf1af901095dca3008d5e0cc2bb
YanaVivcharuk <yanavivcharuk@icloud.com>
2025-11-01 18:01:50 +0100

    hw7

--- src/server.js
+++ src/server.js
@@ -9,9 +9,14 @@ import routes from './routers/index.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import { SWAGGER_PATH } from './constants/index.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const swaggerDocument = JSON.parse(fs.readFileSync(SWAGGER_PATH, 'utf8'));
 
 app.use(cors());
 
@@ -20,7 +25,8 @@ app.use(pino());
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(UPLOAD_DIR));
-app.use('/api-docs', swaggerDocs());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (req, res) => {
   res.send('Welcome to Contacts API!');
@@ -37,6 +43,8 @@ export async function setupServer() {
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+      console.log(`Swagger docs: http://localhost:${PORT}/api-docs`);
     });
   } catch (error) {
     console.log('Error:', error);
